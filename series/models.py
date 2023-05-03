77df82c4dba82324efee22f0709a739812648afa
MichaelMeinhard <tank926@seznam.cz>
2023-05-03 10:31:39 +0200

    2 - model

--- series/models.py
+++ series/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50,
+                            unique=True,
+                            verbose_name='Genre',
+                            help_text='Enter a series genre')
+    description = models.TextField(blank=True,
+                                   null=True)
+
+    class Meta:
+        verbose_name = "Seriál"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
